5489ab08f5233ff4114a43ba3a02d42a16ca7c49
Kevin Chu <kc3852@att.com>
2021-04-16 23:31:44 -0700

    Put remarks to specify which files are used as the data source

--- bikeshare_kc.py
+++ bikeshare_kc.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# data is extracted from the three given .csv files
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
