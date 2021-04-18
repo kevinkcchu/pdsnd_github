dc2bacb9a601aecd4d2e777fca173d7f75065e09
Kevin Chu <kc3852@att.com>
2021-04-17 21:30:37 -0700

    Shorten some text comments in python.py to save the file size

--- bikeshare_kc.py
+++ bikeshare_kc.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington).
     valid_city = ["Chicago", "New York City", "Washington"]
     city = ""
     while city not in valid_city:
@@ -49,16 +49,7 @@ def get_filters():
     return city.lower(), month, day
 
 def load_data(city, month, day):
-    """
-    Loads data for the specified city and filters by month and day if applicable.
 
-    Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
-    """
 # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
 
